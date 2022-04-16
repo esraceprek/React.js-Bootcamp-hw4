9fd64dd75bc5eb64ceb5f9aebc646b395bc2a69a
esraceprek <esraceprek3@gmail.com>
2022-04-17 02:42:21 +0300

    created

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("resim1").innerHTML = " <img src='https://source.unsplash.com/random/id=id+1' />";
+
+document.getElementById("resim2").innerHTML = " <img src='https://source.unsplash.com/random/id=id+2' />";
+
+document.getElementById("resim3").innerHTML = " <img src='https://source.unsplash.com/random/id=id+3' />";
+
+document.getElementById("resim4").innerHTML = " <img src='https://source.unsplash.com/random/id=id+4' />";
+
+document.getElementById("resim5").innerHTML = " <img src='https://source.unsplash.com/random/id=id+5' />";
+
+document.getElementById("resim6").innerHTML = " <img src='https://source.unsplash.com/random/id=id+6' />";
+
+document.getElementById("resim7").innerHTML = " <img src='https://source.unsplash.com/random/id=id+7' />";
+
+document.getElementById("resim8").innerHTML = " <img src='https://source.unsplash.com/random/id=id+8' />";
+
+document.getElementById("resim9").innerHTML = " <img src='https://source.unsplash.com/random/id=id+9' />";
+
+document.getElementById("resim10").innerHTML = " <img src='https://source.unsplash.com/random/id=id+10' />";
+
+document.getElementById("resim11").innerHTML = " <img src='https://source.unsplash.com/random/id=id+11' />";
+
+document.getElementById("resim12").innerHTML = " <img src='https://source.unsplash.com/random/id=id+12' />";
+
+document.getElementById("resim13").innerHTML = " <img src='https://source.unsplash.com/random/id=id+13' />";
+
+document.getElementById("resim14").innerHTML = " <img src='https://source.unsplash.com/random/id=id+14' />";
+
+document.getElementById("resim15").innerHTML = " <img src='https://source.unsplash.com/random/id=id+15' />";
+
+
+
